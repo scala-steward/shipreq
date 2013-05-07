87a9d39b1cca3ef29b5a780f52214579d355eaf9
David Barri <japgolly@gmail.com>
2013-05-07 22:19:30 +1000

    Added normalize.css and jquery.autoresize.js

---
+++ Code/usecase/download-autosize.js.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+o=src/main/webapp/assets/jquery.autosize.js
+curl https://raw.github.com/jackmoore/autosize/master/jquery.autosize.js -o $o \
+  && dos2unix $o
---
+++ Code/usecase/download-normalize.css.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl http://necolas.github.io/normalize.css/2.1.1/normalize.css -o src/main/webapp/assets/normalize.css
