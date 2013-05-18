dfed8713b9a1b1a15ff4a7b05bf4bda3de4e568a
David Barri <japgolly@gmail.com>
2013-05-18 17:06:22 +1000

    Created local & personalised version of jquery-ui

---
+++ Code/usecase/generate-jquery_ui.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+tgt=src/main/webapp/assets/jquery-ui.js
+> $tgt
+
+echo "Adding:"
+for f in \
+  core \
+  effect \
+  effect-drop \
+  effect-fade \
+  effect-highlight \
+  effect-slide \
+; do
+  echo "  $f"
+  cat vendor/jquery-ui/minified/jquery.ui.$f.min.js >> $tgt
+done
+
+echo "Done."
+ls -l $tgt
