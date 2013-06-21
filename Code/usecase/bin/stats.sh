198286dc15c4dc97b0e680f96e88ea18db984e73
David Barri <japgolly@gmail.com>
2013-06-21 19:57:36 +1000

    Added bin/stats.sh

---
+++ Code/usecase/bin/stats.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "main.html       = $(find src/main -name "*.html" | xargs wc -l | tail -1 | perl -pe 's/\D+//g')"
+echo "main.javascript = $(find src/main/javascript -type f | fgrep -v vendor | xargs wc -l | tail -1 | perl -pe 's/\D+//g')"
+echo "main.sass       = $(find src/main/sass -type f | xargs wc -l | tail -1 | perl -pe 's/\D+//g')"
+echo "main.scala      = $(find src/main/scala -type f | xargs wc -l | tail -1 | perl -pe 's/\D+//g')"
+echo "test.scala      = $(find src/test/scala -type f | xargs wc -l | tail -1 | perl -pe 's/\D+//g')"
+
