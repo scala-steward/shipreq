d836c07327b40d301ea428d076eeac508d462d20
David Barri <japgolly@gmail.com>
2013-07-01 14:18:01 +1000

    Added TODOs to bin/stats.sh

--- Code/usecase/bin/stats.sh
+++ Code/usecase/bin/stats.sh
@@ -5,4 +5,6 @@ echo "main.javascript = $(find src/main/javascript -type f | fgrep -v vendor | x
 echo "main.sass       = $(find src/main/sass -type f | xargs wc -l | tail -1 | perl -pe 's/\D+//g')"
 echo "main.scala      = $(find src/main/scala -type f | xargs wc -l | tail -1 | perl -pe 's/\D+//g')"
 echo "test.scala      = $(find src/test/scala -type f | xargs wc -l | tail -1 | perl -pe 's/\D+//g')"
+echo "main.TODOs      = $(find src/main -type f | egrep -v '/_scalate/|vendor' | xargs fgrep TODO | wc -l)"
+echo "test.TODOs      = $(find src/test -type f | egrep -v '/_scalate/|vendor' | xargs fgrep TODO | wc -l)"
 
