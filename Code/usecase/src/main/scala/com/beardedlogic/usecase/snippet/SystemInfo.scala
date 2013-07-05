fd46c5a7ddc761357c21b2fe4c5783c37e7b66c7
David Barri <japgolly@gmail.com>
2013-07-05 18:52:46 +1000

    Created SystemInfo snippet and put to use in layout footer.

---
+++ Code/usecase/src/main/scala/com/beardedlogic/usecase/snippet/SystemInfo.scala
@@ -0,0 +1,32 @@
+package com.beardedlogic.usecase.snippet
+
+import java.util.ResourceBundle
+import net.liftweb.http.S
+import net.liftweb.util.Helpers.strToCssBindPromoter
+import org.joda.time.DateTime
+import scala.util.Properties
+
+object SystemInfo {
+
+  object Build {
+    private val props = ResourceBundle.getBundle("build")
+    private def get(key: String) = props.getString("build." + key)
+    val VersionBase = get("version.base")
+    val VersionFull = get("version.full")
+    val Revision    = get("revision")
+    val TimeStr     = get("time")
+    val Time        = new DateTime(new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(TimeStr))
+  }
+
+  def render = "*" #> value(S.attr("name").openOrThrowException("Attribute 'name' required."))
+
+  def value(name: String): String = name match {
+    case "build.version.base" => Build.VersionBase
+    case "build.version.full" => Build.VersionFull
+    case "build.revision"     => Build.Revision
+    case "build.time"         => Build.TimeStr
+    case "java.version"       => Properties.javaVersion
+    case "jvm.version"        => Properties.javaVmVersion
+    case "scala.version"      => Properties.versionNumberString
+  }
+}
