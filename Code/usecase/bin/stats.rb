0ff29c9b91a5aca521433239661347db6cca2f60
David Barri <japgolly@gmail.com>
2013-08-01 17:45:32 +1000

    Add test JS to stats

--- Code/usecase/bin/stats.rb
+++ Code/usecase/bin/stats.rb
@@ -13,7 +13,8 @@ end
 
 def searchSrc(dp, ratio=true, &cmdFn)
   v = %w[src/main src/test].map{|dir| `#{cmdFn.call(dir)}`.chomp.to_i }
-  r = "%#{dp}d + %#{dp}d = %#{dp}d" % [v[0], v[1], v[0]+v[1]]
+  o = v[0]>v[1] ? ">" : "<"
+  r = "%#{dp}d %s %#{dp}d = %#{dp}d" % [v[0], o, v[1], v[0]+v[1]]
   r += "  (%.2f|%.2f)" % [v[0].to_f/v[1].to_f, v[1].to_f/v[0].to_f] if ratio
   r
 end
@@ -23,15 +24,15 @@ def wcFiles(arg)           arg + "| xargs wc -l | tail -1 | perl -pe 's/\D+//g'"
 def wcFilesInDir(dir)      wcFiles("find #{dir} -type f") end
 def wcFilesByType(dir,pat) wcFiles("find #{dir} -name '#{pat}'") end
 
-puts "main.html       - #{searchMain(5){ wcFilesByType 'src/main', '*.html' }}"
-puts "main.scaml      - #{searchMain(5){ wcFilesByType 'src/main', '*.scaml' }}"
-puts "main.javascript - #{searchMain(5){ wcFiles "find src/main/javascript -type f | fgrep -v vendor" }}"
-puts "main.sass       - #{searchMain(5){ wcFilesInDir 'src/main/sass' }}"
+puts "main.html  - #{searchMain(5){ wcFilesByType 'src/main', '*.html' }}"
+puts "main.scaml - #{searchMain(5){ wcFilesByType 'src/main', '*.scaml' }}"
+puts "main.sass  - #{searchMain(5){ wcFilesInDir 'src/main/sass' }}"
 puts
-puts "Scala - #{searchSrc(5){|dir| wcFilesInDir("#{dir}/scala") }}"
+puts "Scala      - #{searchSrc(5){|dir| wcFilesInDir("#{dir}/scala") }}"
+puts "JavaScript - #{searchSrc(5){|dir| wcFiles "find #{dir}/javascript -name '*.js' | fgrep -v vendor" }}"
 puts
-puts "TODO.Func - #{countTodoFileContents `cat TODO.md | sed '0,/FUNC TODO/d; /TECH TODO/,$d'`.split(/[\r\n]+/)}"
-puts "TODO.Tech - #{countTodoFileContents `cat TODO.md | sed '0,/TECH TODO/d'`.split(/[\r\n]+/)}"
-puts "TODO.src  -   #{searchSrc(3){|dir| wcStdout("find #{dir} -type f | egrep -v '/_scalate/|vendor' | xargs fgrep TODO") }}"
+puts "TODO.Func  - #{countTodoFileContents `cat TODO.md | sed '0,/FUNC TODO/d; /TECH TODO/,$d'`.split(/[\r\n]+/)}"
+puts "TODO.Tech  - #{countTodoFileContents `cat TODO.md | sed '0,/TECH TODO/d'`.split(/[\r\n]+/)}"
+puts "TODO.src   -   #{searchSrc(3){|dir| wcStdout("find #{dir} -type f | egrep -v '/_scalate/|vendor' | xargs fgrep TODO") }}"
 
 
