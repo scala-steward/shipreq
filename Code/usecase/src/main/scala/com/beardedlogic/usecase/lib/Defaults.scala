0f5d98a65d83e9e0db5099872f9fc4084458c0ca
David Barri <japgolly@gmail.com>
2013-08-09 23:45:27 +1000

    Fix Defaults.

--- Code/usecase/src/main/scala/com/beardedlogic/usecase/lib/Defaults.scala
+++ Code/usecase/src/main/scala/com/beardedlogic/usecase/lib/Defaults.scala
@@ -6,14 +6,6 @@ import field._
 import model._
 import util.LazyVal
 
-/**
- * Data IDs below 100 are reserved and can be safely allocated here.
- */
-object ReservedIds {
-
-  val DefaultFieldList = 1
-}
-
 object Defaults extends Logger {
 
   /** Default title of new use cases. */
@@ -33,11 +25,7 @@ object Defaults extends Logger {
       TextFieldDefinition("Notes and Issues") ::
       Nil
 
-  val FieldList: LazyVal[FieldListRec] = LazyDbVal(dao => {
-    val fl = dao.syncFieldList(ReservedIds.DefaultFieldList, FieldListDefns)
-    debug(s"Default field list: ${fl.dataId}:${fl.valueId}")
-    fl
-  })
+  val FieldList: LazyVal[FieldListRec] = LazyDbVal(_.syncFieldList(FieldListDefns))
 
   private def LazyDbVal[V](fn: DAO => V) = LazyVal <~ DI.DaoProvider.withTransaction(fn)
 
