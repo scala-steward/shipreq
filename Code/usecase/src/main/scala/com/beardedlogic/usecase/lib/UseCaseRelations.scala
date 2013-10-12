67953be805f8706fc127cdc56258d46df6d61415
David Barri <japgolly@gmail.com>
2013-10-13 10:42:04 +1100

    Tiny clean-up of UseCaseRelations

--- Code/usecase/src/main/scala/com/beardedlogic/usecase/lib/UseCaseRelations.scala
+++ Code/usecase/src/main/scala/com/beardedlogic/usecase/lib/UseCaseRelations.scala
@@ -1,19 +1,18 @@
 package com.beardedlogic.usecase.lib
 
+import com.beardedlogic.usecase.db.UseCaseSummary
 import Types._
-import com.beardedlogic.usecase.db.{UseCaseSummary, DaoT}
 
 trait UseCaseRelations {
-
   def findUcTitle(num: UseCaseNumber): Option[String]
-}
-
-case class CachedUseCaseRelations(ucs: List[UseCaseSummary]) extends UseCaseRelations {
-  override def findUcTitle(num: UseCaseNumber) = ucs.find(_.number == num).map(_.title)
 }
 
 object UseCaseRelations {
   val Empty: UseCaseRelations = new UseCaseRelations {
     override def findUcTitle(num: UseCaseNumber) = None
   }
+}
+
+case class CachedUseCaseRelations(ucs: List[UseCaseSummary]) extends UseCaseRelations {
+  override def findUcTitle(num: UseCaseNumber) = ucs.find(_.number == num).map(_.title)
 }
