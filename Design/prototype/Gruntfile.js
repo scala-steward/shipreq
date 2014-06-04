2e63f7ee95e642bc41a1e656ac440bb7975c558f
David Barri <japgolly@gmail.com>
2014-06-04 19:02:05 +1000

    Prototype now uses coffeescript

--- Design/prototype/Gruntfile.js
+++ Design/prototype/Gruntfile.js
@@ -4,35 +4,34 @@ module.exports = function(grunt) {
   // Load all the Grunt tasks listed in package.json
   require('matchdep').filterDev('grunt-*').forEach( grunt.loadNpmTasks );
 
+  // ===========================================================================
   grunt.initConfig({
 
-    // *************************************************************************
     haml: {
       all: {
-        files: [{
-          expand: true,
-          cwd: 'src/',
-          src: ['**/*.haml'],
-          dest: 'src/',
-          ext: '.html',
-          flatten: false,
-        }],
+        files: [{expand: true, cwd: 'src/', src: ['**/*.haml'], dest: 'src/', ext: '.html', flatten: false }],
       },
     },
 
+    coffee: {
+      all: {
+        files: [{expand: true, cwd: 'src/', src: ['**/*.coffee'], dest: 'src/', ext: '-coffee.js', flatten: false }],
+      },
+    },
 
-    // *************************************************************************
     watch: {
       options: {
         spawn: false,
       },
-      haml: {
-        files: ['src/**/*.haml'],
-        tasks: ['haml'],
-      },
+      haml:   { files: ['src/**/*.haml'],   tasks: ['haml'] },
+      coffee: { files: ['src/**/*.coffee'], tasks: ['coffee'] },
     },
 
   });
+
+  // ===========================================================================
+  grunt.registerTask('default', ['haml', 'coffee']);
+
 };
 
 // vim:sw=2 ts=2 et:
