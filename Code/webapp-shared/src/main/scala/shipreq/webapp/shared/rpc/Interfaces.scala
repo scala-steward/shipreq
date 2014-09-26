c428c5d2b859a9b09d283246d1e82c1903f1e49b
David Barri <japgolly@gmail.com>
2014-09-26 23:16:37 +1000

    Interface.Cluster JSON obfuscation

--- Code/webapp-shared/src/main/scala/shipreq/webapp/shared/rpc/Interfaces.scala
+++ Code/webapp-shared/src/main/scala/shipreq/webapp/shared/rpc/Interfaces.scala
@@ -1,6 +1,7 @@
 package shipreq.webapp.shared.rpc
 
 import Interface._
+import upickle.key
 
 object Interfaces {
 
@@ -8,9 +9,9 @@ object Interfaces {
   object Half extends DefT[Int, String]
   object Grrr extends DefT[ExampleData, ExampleData]
 
-  case class WIP(square: Square.Remote,
-                 half: Half.Remote,
-                 grrr: Grrr.Remote) extends Cluster
+  case class WIP(@key("a") square: Square.Remote,
+                 @key("b") half: Half.Remote,
+                 @key("c") grrr: Grrr.Remote) extends Cluster
 
 }
 
