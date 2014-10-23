c93ee4c700a9bc0ff33dc79acd9e7aea8b5edd5b
David Barri <japgolly@gmail.com>
2014-10-24 09:40:37 +1100

    Router for cfg pages

--- Code/webapp-client/src/main/scala/ReactExamples.scala
+++ Code/webapp-client/src/main/scala/ReactExamples.scala
@@ -11,26 +11,68 @@ import shipreq.webapp.base.protocol.Routines
 import shipreq.webapp.client.lib._
 import shipreq.webapp.client.ui._
 import shipreq.webapp.client.util.route._
+import japgolly.scalajs.react._, vdom.ReactVDom._, all._, ScalazReact._, experiment._
 
 object ReactExamples {
 
   def main(r: Routines.ForCfgReqType) = IO[Unit] {
     example1(document getElementById "eg1")
+//    manual()
+    projectPage(r).unsafePerformIO()
+  }
 
-    manual()
+  def addBack[P](root: Root[P], inner: Renderer[P]): Renderer[P] = router => {
+    val c = ReactComponentB[Unit]("Outer")
+      .render(_ =>
+      div(
+        div(backgroundColor := "#ddd", router.link(root)("Back")),
+        inner(router))
+      ).buildU
+    c()
+  }
 
+  // ===================================================================================================================
+
+  def projectPage(r: Routines.ForCfgReqType): IO[Unit] = {
     import shipreq.webapp.client._
     ClientData.init(r.projectInit, clientData => IO {
 
+      sealed trait ProjectPage
+      object ProjectPage extends Page[ProjectPage] {
+        val root = Root(index)
+        val cfgIncmp    = path("#cfg/incmp",    addBack(root, cfgIncmpR))
+        val cfgReqTypes = path("#cfg/reqtypes", addBack(root, cfgReqTypesR))
+      }
 
-//      CfgReqTypes.comp(TableIoProps(r.reqTypeCrud, clientData, false)) render document.getElementById("eg2")
-//      CfgIncompletions.comp(CfgIncompletions.Props(r.incmpCrud, r.reqTypeImpMod, clientData, false)) render document.getElementById("eg3")
-    }).unsafePerformIO()
+      def index: Renderer[ProjectPage] = router => {
+        val c = ReactComponentB[Unit]("Index")
+          .render(_ =>
+          ul(
+            li(router.link(ProjectPage.cfgIncmp)("Cfg: Incompletions")),
+            li(router.link(ProjectPage.cfgReqTypes)("Cfg: Requirement Types")))
+          ).buildU
+        c()
+      }
+
+      def cfgIncmpR: Renderer[ProjectPage] = _ =>
+        CfgIncompletions.comp(CfgIncompletions.Props(r.incmpCrud, r.reqTypeImpMod, clientData, false))
+
+      def cfgReqTypesR: Renderer[ProjectPage] = _ =>
+        CfgReqTypes.comp(TableIoProps(r.reqTypeCrud, clientData, false))
+
+      val c = Router.component(BaseUrl("/wip"), ProjectPage)
+      c() render document.getElementById("eg2")
+    })
   }
 
-    // ===================================================================================================================
+  // ===================================================================================================================
 
-  import japgolly.scalajs.react._, vdom.ReactVDom._, all._, ScalazReact._
+  def example1(mountNode: Node) = {
+    val HelloMessage = ReactComponentB[String]("HelloMessage")
+      .render(name => div("Hello ", name))
+      .build
+    React.renderComponent(HelloMessage("John"), mountNode)
+  }
 
   def manual() = {
     val tgt = document.getElementById("eg2")
@@ -38,18 +80,8 @@ object ReactExamples {
     sealed trait MyPage
     object MyPage extends Page[MyPage] {
       val root = Root(RootC)
-      val f2 = path("#f2", addBack(Route2C))
-      val f3 = path("#f3", addBack(Route3C))
-    }
-
-    def addBack(inner: Renderer[MyPage]): Renderer[MyPage] = router => {
-      val c = ReactComponentB[Unit]("Outer")
-        .render(_ =>
-        div(
-          div(backgroundColor := "#ddd", router.link(MyPage.root)("Back")),
-          inner(router))
-      ).buildU
-      c()
+      val f2 = path("#f2", addBack(root, Route2C))
+      val f3 = path("#f3", addBack(root, Route3C))
     }
 
     def RootC: Renderer[MyPage] = router => {
@@ -59,13 +91,16 @@ object ReactExamples {
           h2("Top Level. Top Secret."),
           div(router.link(MyPage.f2)("F222222222222222222222222")),
           div(router.link(MyPage.f3)("F333333333333333333333333")))
-        ).buildU
+        )
+        .configure(LogLifecycle.short)
+        .buildU
       c()
     }
 
     def Route2C: Renderer[MyPage] = router => {
       val c = ReactComponentB[Unit]("F2")
         .render(_ => div(h3("Cool.")))
+        .configure(LogLifecycle.short)
         .buildU
       c()
     }
@@ -73,6 +108,7 @@ object ReactExamples {
     def Route3C: Renderer[MyPage] = router => {
       val c = ReactComponentB[String]("F3")
         .render(p => div(h3("Hello ", p)))
+        .configure(LogLifecycle.short)
         .build
       c("hehe cool")
     }
@@ -81,12 +117,4 @@ object ReactExamples {
     React.renderComponent(c(), tgt)
   }
 
-  def example1(mountNode: Node) = {
-
-    val HelloMessage = ReactComponentB[String]("HelloMessage")
-      .render(name => div("Hello ", name))
-      .build
-
-    React.renderComponent(HelloMessage("John"), mountNode)
-  }
 }
