031bbd42c888c59c21485c514eea2938ceb22796
David Barri <japgolly@gmail.com>
2014-10-31 23:27:35 +1100

    CycleDetector

---
+++ Code/prop-core/src/main/scala/shipreq/prop/CycleDetector.scala
@@ -0,0 +1,74 @@
+package shipreq.prop
+
+import scala.annotation.tailrec
+import scalaz.{\/-, -\/, \/}
+
+case class CycleDetector[-A, B](extract: A => Stream[B], check: (A, Stream[B]) => Option[(B, B)]) {
+
+  @inline final def hasCycle(a: A): Boolean =
+    findCycle(a).isDefined
+
+  @inline final def noCycle(a: A): Boolean =
+    findCycle(a).isEmpty
+
+  final def findCycle(a: A): Option[(B, B)] =
+    check(a, extract(a))
+
+  def contramap[Z](f: Z => A) =
+    new CycleDetector[Z, B](extract compose f, (z, b) => check(f(z), b))
+}
+
+object CycleDetector {
+
+  object Undirected extends GraphType {
+    override def check[A, B, I](vertices: (A, B) => Stream[B], id: B => I): (A, Stream[B]) => Option[(B, B)] =
+      (a, input) => {
+        def loopo = loop _
+        @tailrec
+        def loop(prev: B, bs: Stream[B], is: Set[I]): Set[I] \/ (B, B) = {
+          bs match {
+            case Stream.Empty => -\/(is)
+            case b #:: t =>
+              val i = id(b)
+              if (is contains i)
+                \/-((prev, b))
+              else
+                loopo(b, vertices(a, b), is + i) match {
+                  case -\/(is2)  => loop(prev, t, is2)
+                  case r@ \/-(_) => r
+                }
+          }
+        }
+        input.map(b => loop(b, vertices(a, b), Set(id(b)))).map(_.toOption).find(_.isDefined).flatten
+      }
+  }
+
+  object Directed extends GraphType {
+    override def check[A, B, I](vertices: (A, B) => Stream[B], id: B => I): (A, Stream[B]) => Option[(B, B)] =
+      (a, input) => {
+        def loop(prev: B, bs: Stream[B], is: Set[I]): Option[(B, B)] = {
+          bs.map(b => {
+            val i = id(b)
+            if (is contains i)
+              Some((prev, b))
+            else
+              loop(b, vertices(a, b), is + i)
+          }).find(_.isDefined).flatten
+        }
+        input.map(b => loop(b, vertices(a, b), Set(id(b)))).find(_.isDefined).flatten
+      }
+  }
+
+  sealed abstract class GraphType {
+    def check[A, B, I](vertices: (A, B) => Stream[B], id: B => I): (A, Stream[B]) => Option[(B, B)]
+
+    def tree[A, I](vertices: A => Stream[A], id: A => I) = CycleDetector[Stream[A], A](
+      identity, check((_, a) => vertices(a), id))
+
+    def map[A, I](id: A => I) = CycleDetector[Map[A, A], A](
+      _.keys.toStream, check(_.get(_).toStream, id))
+
+    def setmultimap[A, I](id: A => I) = CycleDetector[Map[A, Set[A]], A](
+      _.keys.toStream, check(_.getOrElse(_, Set.empty).toStream, id))
+  }
+}
