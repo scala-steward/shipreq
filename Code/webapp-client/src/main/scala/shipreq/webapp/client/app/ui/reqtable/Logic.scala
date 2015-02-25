0acf3fe4d41e108f8dbdb44ebaf2ae893acdd53d
David Barri <japgolly@gmail.com>
2015-02-25 16:03:20 +1100

    Bit more lazyness to Logic

--- Code/webapp-client/src/main/scala/shipreq/webapp/client/app/ui/reqtable/Logic.scala
+++ Code/webapp-client/src/main/scala/shipreq/webapp/client/app/ui/reqtable/Logic.scala
@@ -6,6 +6,7 @@ import shipreq.webapp.base.data._
 private[reqtable] object Logic {
 
   private type Expanded[+A] = NonEmptyList[List[A]]
+  private type Expander[A]  = (() => Set[A]) => Expanded[A]
 
   private final val emptyExpansions: NonEmptyList[Expansion] =
     NonEmptyList(Expansion.none)
@@ -24,20 +25,18 @@ private[reqtable] object Logic {
    * @param visible Is the column visible? If not, just pretend everything is empty.
    * @param expand  When visible, does the data need to be expanded?
    */
-  private def expander[A](visible: Boolean, expand: => Boolean): Set[A] => Expanded[A] = {
-    type F = Set[A] => Expanded[A]
-
-    @inline def nonEmpty(f: (A, List[A]) => Expanded[A]): F =
-      _.toList match {
+  private def expander[A](visible: Boolean, expand: => Boolean): Expander[A] = {
+    @inline def nonEmpty(f: (A, List[A]) => Expanded[A]): Expander[A] =
+      _().toList match {
         case Nil    => emptyExpanded
         case h :: t => f(h, t)
       }
 
-    def doExpand: F =
+    def doExpand: Expander[A] =
       nonEmpty((h, t) =>
         NonEmptyList.nel(h :: Nil, t.map(_ :: Nil)))
 
-    def dontExpand: F =
+    def dontExpand: Expander[A] =
       nonEmpty((h, t) => NonEmptyList(h :: t))
 
     if (visible) {
@@ -46,7 +45,7 @@ private[reqtable] object Logic {
       _ => emptyExpanded
   }
 
-  private def expanderC[A](vs: ViewSettings, c: Column.SortInconclusive): Set[A] => Expanded[A] =
+  private def expanderC[A](vs: ViewSettings, c: Column.SortInconclusive): Expander[A] =
     expander(vs isVisible c, vs isOrdered c)
 
   private def expansions(impSrcs: Expanded[Req.Id], impTgts: Expanded[Req.Id], codes: Expanded[ReqCode]): NonEmptyList[Expansion] =
@@ -83,9 +82,9 @@ private[reqtable] object Logic {
         // Filter
 
         // Expansion
-        val impSrcs = expandImpSrcs(pImplications.tgtToSrc(id))
-        val impTgts = expandImpTgts(pImplications.srcToTgt(id))
-        val codes   = expandCodes(pReqCodes.byTarget(id))
+        val impSrcs = expandImpSrcs(() => pImplications.tgtToSrc(id))
+        val impTgts = expandImpTgts(() => pImplications.srcToTgt(id))
+        val codes   = expandCodes  (() => pReqCodes.byTarget(id))
         val exps    = expansions(impSrcs, impTgts, codes)
 
         // Done
