caacb560894bfa16c665a4b5e691234aedd28615
David Barri <japgolly@gmail.com>
2015-07-01 16:51:47 +1000

    InternalHash

---
+++ Code/webapp-base/shared/src/main/scala/shipreq/webapp/base/hash/InternalHash.scala
@@ -0,0 +1,19 @@
+package shipreq.webapp.base.hash
+
+/**
+ * Hash just using values' internal hash codes.
+ *
+ * Useless really but here for playing around.
+ */
+object InternalHash extends Hash.Algorithm with Hash.Primitives {
+
+  implicit override val hashString                   : Hash[String]    = Hash.internal
+  override implicit def hashPair   [A: Hash, B: Hash]: Hash[(A, B)]    = Hash.internal
+  implicit override def hashMap    [K: Hash, V: Hash]: Hash[Map[K, V]] = Hash.internal
+  implicit override def hashSet    [A: Hash]         : Hash[Set[A]]    = Hash.internal
+  implicit override def hashList   [A: Hash]         : Hash[List[A]]   = Hash.internal
+  implicit override def hashVector [A: Hash]         : Hash[Vector[A]] = Hash.internal
+
+  override def joinHashes(hashes: List[Int]) = hashes.##
+  override def hashUnordered[T[x] <: TraversableOnce[x], A: Hash]: Hash[T[A]] = Hash.internal
+}
