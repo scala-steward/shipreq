3e5be84b8a5c464502e529a7995b7c8cbecf7057
David Barri <japgolly@gmail.com>
2015-07-02 11:08:43 +1000

    More hash testing

---
+++ Code/webapp-base-test/shared/src/test/scala/shipreq/webapp/base/hash/HashTest.scala
@@ -0,0 +1,130 @@
+package shipreq.webapp.base.hash
+
+import upickle._
+import upickle.Fns._
+import japgolly.nyaya._
+import japgolly.nyaya.util.NyayaUtilAnyExt
+import japgolly.nyaya.test._
+import japgolly.nyaya.test.PropTestOps._
+import utest._
+
+import shipreq.webapp.base.test.BaseTestUtil.assertEq
+import shipreq.base.util.UnivEq.int
+import shipreq.webapp.base.RandomData
+import shipreq.webapp.base.data._
+import shipreq.webapp.base.protocol.DataCodecs._
+import Hash.HashableValueOps
+
+object HashTest extends TestSuite {
+
+  implicit val settings = DefaultSettings.propSettings
+    .setSampleSize(80 `JVM|JS` 40).setGenSize(20)
+//    .setDebug
+
+  // ===================================================================================================================
+  // Hash algorithms
+
+  val nv = (0 to 10 by 2).toVector
+  val ef = nv.map(_.toString)
+  val of = nv.map(i => (i + 1).toString)
+  val v1f = ef ++ of
+  val v2f = of ++ ef
+  val v3f = (0 to 11).toVector.map(_.toString)
+  val v1r = v1f.reverse
+  val v2r = v2f.reverse
+  val v3r = v3f.reverse
+  val vs  = Vector(v1f, v1r, v2f, v2r, v3f, v3r)
+
+  def algorithmProp(expect: AlgorithmResults): Prop[Hash.Algorithm] = {
+    type A = Hash.Algorithm
+
+    def sameResultsOnJvmAndJs = Prop.equal[A]("Same Results on JVM & JS")(
+      AlgorithmResults.calc,
+      _ => expect)
+
+    def unorderedHashIgnoresOrder = Prop.atom[A]("unorderedHash ignores order", a => {
+      import a.hashString
+      val h = a.hashUnordered[Vector, String]
+      val hs = vs.map(h.hash).toSet
+      if (hs.size == 1)
+        None
+      else
+        Some(s"${hs.size} distinct hashes returned.")
+    })
+
+    def noCollisions[B](name: String, f: A => Hash[B])(b1: B, b2: B, bn: B*): Prop[A] =
+      Prop.atom[A](s"hashValuesDiffer: $name", a => {
+        val bs = bn.toVector :+ b1 :+ b2
+        val h  = f(a)
+        var hs = Set.empty[Int]
+        bs foreach (hs += h hash _)
+        val c = bs.size - hs.size
+        if (c == 0)
+          None
+        else
+          Some(s"$c collisions detected.")
+      })
+
+    def hashFnsVary = (
+      noCollisions("int"          , _.hashInt                                )(3, 4)
+    ∧ noCollisions("long"         , _.hashLong                               )(3L, 4L)
+    ∧ noCollisions("string"       , _.hashString                             )("a", "aa", "b")
+    ∧ noCollisions("pair"         , a ⇒ a.hashPair(a.hashInt, a.hashString)  )((4, "a"), (5, "a"), (4, "b"), (5, "b"))
+    ∧ noCollisions("map"          , a ⇒ a.hashMap(a.hashInt, a.hashString)   )(Map(4 -> "f", 6 -> "s"), Map(6 -> "s"), Map(3 -> "f", 6 -> "s"))
+    ∧ noCollisions("set"          , a ⇒ a.hashSet(a.hashString)              )(Set("a"), Set("b"), Set("a", "b"))
+    ∧ noCollisions("list"         , a ⇒ a.hashList(a.hashString)             )(List("a"), List("b"), List("a", "b"), List("b", "a"))
+    ∧ noCollisions("vector"       , a ⇒ a.hashVector(a.hashString)           )(Vector("a"), Vector("b"), Vector("a", "b"), Vector("b", "a"))
+    ∧ noCollisions("hashUnordered", a ⇒ a.hashUnordered[List, Int](a.hashInt))(List(1), List(2), List(1, 2))
+    ) rename "hashFnsVary"
+
+    sameResultsOnJvmAndJs & unorderedHashIgnoresOrder & hashFnsVary
+  }
+
+  // Ensure that JVM & JS produce the same results
+  def testAlgo(a: Hash.Algorithm, expect: AlgorithmResults): Unit =
+    algorithmProp(expect).assert(a)
+
+  val murmur3 = AlgorithmResults(1231,1237,1000,-1294967296,1842429670,381277126,-914897307,-1390910323,586134407,2075563892,-1936667874,-1122530123)
+
+  // ===================================================================================================================
+  // Data hashing
+
+  import HashScheme.default._
+
+  case class DataHashTest[A: Hash : Reader : Writer](a1: A, a2: A, a3: A) {
+    val E = EvalOver(this)
+
+    val h1 = a1.hash
+    val h2 = a2.hash
+    val h3 = a3.hash
+
+    def consistent(h: Int, a: A) =
+      E.equal("consistent: hash(a) = hash(deser(ser(a)))", h, readJs[A](writeJs(a)).hash)
+
+    def allConsistent =
+      consistent(h1, a1) ∧ consistent(h2, a2) ∧ consistent(h3, a3)
+
+    def hashesDiffer =
+      E.test("Hashes must differ", Set(h1, h2, h3).size >= 2)
+
+    def main =
+      allConsistent ∧ hashesDiffer
+  }
+
+  def dataHashTest[A] = Prop.eval[DataHashTest[A]](_.main)
+
+  def testData[A: Hash : Reader : Writer](g: Gen[A]): Unit = {
+    val t = for {a <- g; b <- g; c <- g} yield new DataHashTest(a, b, c)
+    dataHashTest[A] mustBeSatisfiedBy t
+  }
+
+  // ===================================================================================================================
+  override def tests = TestSuite {
+    'algorithms {
+      'murmur3 - testAlgo(MurmurHash3, murmur3)
+    }
+    'data {
+      'project - testData(RandomData.project)
+    }
+  }
+}
