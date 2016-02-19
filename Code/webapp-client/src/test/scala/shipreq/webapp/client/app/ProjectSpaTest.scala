007af3167ef80cefaa29f640d2454ddf01314004
David Barri <japgolly@gmail.com>
2016-02-19 17:34:34 +1100

    slow beginning

---
+++ Code/webapp-client/src/test/scala/shipreq/webapp/client/app/ProjectSpaTest.scala
@@ -0,0 +1,20 @@
+package shipreq.webapp.client.app
+
+import shipreq.webapp.base.test._
+import shipreq.webapp.client.app.state.ClientData
+import shipreq.webapp.client.test._
+import utest._
+
+object ProjectSpaTest extends TestSuite {
+  PrepareEnv()
+
+  def runTest = {
+    val cp = new TestClientProtocol
+    val cd = new ClientData(SampleProject3.project)
+    val spa = new ProjectSpaMain(MockRemotes.projectSPA, cp, cd)
+  }
+
+  override def tests = TestSuite {
+
+  }
+}
