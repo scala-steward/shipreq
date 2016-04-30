fedc44fea3b4d02fbd6a132438b92c6bd90cfaa2
David Barri <japgolly@gmail.com>
2016-04-30 17:15:35 +1000

    Add Debug.time

--- Code/base-util/shared/src/main/scala/shipreq/base/util/Debug.scala
+++ Code/base-util/shared/src/main/scala/shipreq/base/util/Debug.scala
@@ -1,5 +1,7 @@
 package shipreq.base.util
 
+import scala.annotation.elidable
+
 /**
  * Methods to help with debugging.
  */
@@ -9,6 +11,28 @@ object Debug {
     def pp(): T = {println(v); v}
     def pp(title: Any): T = {println(s"$title: $v"); v}
     def pp(f: T => Any): T = {println(f(v)); v}
+  }
+
+  def time[A](name: String)(a: => A): A = {
+    val start  = __start
+    val result = a
+    __stop(name, start)
+    result
+  }
+
+  import System.{nanoTime => clock}
+
+  private type Clock = Long
+
+  @elidable(elidable.FINE)
+  private def __start: Clock =
+    clock()
+
+  @elidable(elidable.FINE)
+  private def __stop(name: String, start: Clock): Unit = {
+    val end = clock()
+    val timeInMs = (end - start).toDouble / 1000000
+    printf("[%s] Completed in %.3f ms.\n", name, timeInMs)
   }
 }
 
