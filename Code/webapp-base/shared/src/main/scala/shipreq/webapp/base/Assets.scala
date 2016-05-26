f7d8401bc90a3cf8f08ecfdfbc0d3cf4558a624c
David Barri <japgolly@gmail.com>
2016-05-26 16:10:40 +1000

    Create shipreq.webapp.base.Assets

---
+++ Code/webapp-base/shared/src/main/scala/shipreq/webapp/base/Assets.scala
@@ -0,0 +1,9 @@
+package shipreq.webapp.base
+
+import WebappConfig.assetPath_/
+
+object Assets {
+  lazy val SvgSortAsc   = assetPath_/ + "sort-asc.svg"
+  lazy val SvgSortBlank = assetPath_/ + "sort-blank.svg"
+  lazy val SvgSpinner   = assetPath_/ + "loading-spin.svg"
+}
--- Code/webapp-client-project/src/main/scala/shipreq/webapp/client/project/app/Assets.scala
+++ Code/webapp-client-project/src/main/scala/shipreq/webapp/client/project/app/Assets.scala
@@ -1,15 +1,12 @@
 package shipreq.webapp.client.project.app
 
 import japgolly.scalajs.react.vdom.prefix_<^._
-import shipreq.webapp.base.WebappConfig.assetPath
+import shipreq.webapp.base.Assets._
 
 object Assets {
-  val sortSvgAsc   = <.img(^.src := s"$assetPath/sort-asc.svg", ^.alt := "Asc")
-  val sortSvgDesc  = <.img(^.src := s"$assetPath/sort-asc.svg", ^.alt := "Desc", ^.transform := "scaleY(-1)")
-  val sortSvgBlank = <.img(^.src := s"$assetPath/sort-blank.svg", ^.alt := "Blanks")
+  val sortSvgAsc   = <.img(^.src := SvgSortAsc, ^.alt := "Asc")
+  val sortSvgDesc  = <.img(^.src := SvgSortAsc, ^.alt := "Desc", ^.transform := "scaleY(-1)")
+  val sortSvgBlank = <.img(^.src := SvgSortBlank, ^.alt := "Blanks")
 
-  val spinner =
-    <.img(
-      ^.cls := "spinner",
-      ^.src := s"$assetPath/loading-spin.svg")
+  val spinner = <.img(^.src := SvgSpinner, ^.cls := "spinner")
 }
