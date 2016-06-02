c22e3d3b965229b67c4cd9a7c27396f4f9a35c8e
David Barri <japgolly@gmail.com>
2016-06-02 16:32:19 +1000

    Reenable ExternalIdTest

--- Code/webapp-server/src/test/scala/shipreq/webapp/server/util/ExternalIdTest.scala
+++ Code/webapp-server/src/test/scala/shipreq/webapp/server/util/ExternalIdTest.scala
@@ -1,40 +1,36 @@
 package shipreq.webapp.server.util
 
-//import org.scalacheck.Prop._
-//import org.scalatest.FunSuite
-//import org.scalatest.prop.Checkers
-//import org.scalatest.Matchers
-//import shipreq.webapp.server.lib.Types._
-//
-//class ExternalIdTest extends FunSuite with Checkers with Matchers {
-//
-//  implicit override val generatorDrivenConfig = PropertyCheckConfig(minSuccessful = 10000)
-//
-//  test("Conversion back and fro") {
-//    check {
-//      id: Long =>
-//        val ext = toExternal(id)
-//        val i2 = parseO(ext).get
-//        (i2.value == id) :| s"$id --> $ext --> $i2"
-//    }
-//  }
-//
-//  test("External string format") {
-//    check {
-//      id: Long =>
-//        toExternal(id).value should fullyMatch regex "^[a-zA-Z0-9]{4,12}$"
-//        true
-//    }
-//  }
-//
-//  test("External strings should not be sequential") {
-//    val lastCh = "(.)$".r
-//    check {
-//      id: Long =>
-//        val a = toExternal(id)
-//        val b = toExternal(id + 1)
-//        val a2 = lastCh.replaceSomeIn(a, m => Some((m.group(1)(0) + 1).toChar.toString))
-//        (a2 != b.value) :| s"$a + 1 matches $b"
-//    }
-//  }
-//}
+import nyaya.gen._
+import nyaya.prop._
+import nyaya.test.PropTest._
+import utest._
+import shipreq.base.test.BaseTestUtil._
+
+object ExternalIdTest extends TestSuite {
+
+  val dictStr = scala.util.Random.shuffle("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".toList).mkString
+  val scheme = ExternalId.scheme[Any, Long](identity, identity, dictStr)
+  val extFmt = "^[a-zA-Z0-9]{4,12}$".r.pattern
+  val lastCh = "(.)$".r
+
+  class Test(id: Long) {
+    val E = EvalOver(s"$id * $dictStr")
+
+    val ext = scheme(id).value
+
+    val result =
+      E.equal("encode.decode = id", scheme.parseO(ext), Some(id)) &
+      E.test("Format", extFmt.matcher(ext).matches) &
+      E.test("Non-sequential", {
+        val e1b = lastCh.replaceSomeIn(ext, m => Some((m.group(1)(0) + 1).toChar.toString))
+        val e2 = scheme(id + 1).value
+        e1b !=* e2
+      })
+  }
+
+  val prop = Prop.eval[Long](new Test(_).result)
+
+  override def tests = TestSuite {
+    Gen.long mustSatisfy prop
+  }
+}
