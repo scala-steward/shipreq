f9aef5922e93f151e909681f61553a9d69e96ca4
David Barri <japgolly@gmail.com>
2019-05-12 10:00:02 +1000

    Add jprofiler config (commented out)

--- Code/project/WebappBuild.scala
+++ Code/project/WebappBuild.scala
@@ -309,6 +309,7 @@ object WebappBuild {
         containerLibs in Jetty := LibJetty.devRun(JVM),
         javaOptions in Jetty += "-Xmx1g",
         javaOptions in Jetty += "-XX:+UseG1GC", // Default in Java 9, may as well use it now
+        // javaOptions in Jetty += "-agentpath:/opt/jprofiler10/bin/linux-x64/libjprofilerti.so=port=8849,nowait",
         initialCommands += consoleCmds,
         fullClasspath in console in Compile += file("src/main/webapp")) // So templates can be loaded from console
   }
