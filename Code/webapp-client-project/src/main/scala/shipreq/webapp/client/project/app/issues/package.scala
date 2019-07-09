e53b42b1e5cc247dbd1660462fb30760707fe206
David Barri <japgolly@gmail.com>
2019-07-09 18:19:42 +1000

    Fix comment

--- Code/webapp-client-project/src/main/scala/shipreq/webapp/client/project/app/issues/package.scala
+++ Code/webapp-client-project/src/main/scala/shipreq/webapp/client/project/app/issues/package.scala
@@ -5,6 +5,6 @@ import japgolly.scalajs.react._
 package object issues {
   @inline def shouldComponentUpdate[P: Reusability, C <: Children, S: Reusability, B, U <: UpdateSnapshot]: ScalaComponent.Config[P, C, S, B, U, U] =
     shipreq.webapp.client.project.app.shouldComponentUpdate[P, C, S, B, U]
-//   ReusabilityOverlay.install[P, C, S, B]
+    // japgolly.scalajs.react.extra.ReusabilityOverlay.install[P, C, S, B, U]
 
 }
