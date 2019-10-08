92d52821352805205a446c6a000dd94aaf3e9e9b
David Barri <japgolly@gmail.com>
2019-10-08 11:31:16 +1100

    Upgrade Terraform to 0.12

--- Infrastructure/terraform/global/dns.tf
+++ Infrastructure/terraform/global/dns.tf
@@ -4,7 +4,7 @@ resource "aws_route53_zone" "shipreq" {
 
 # www.shipreq.com -> shipreq.com
 resource "aws_route53_record" "www" {
-  zone_id = "${aws_route53_zone.shipreq.zone_id}"
+  zone_id = aws_route53_zone.shipreq.zone_id
   name    = "www.shipreq.com."
   type    = "CNAME"
   ttl     = "21600"
@@ -12,7 +12,7 @@ resource "aws_route53_record" "www" {
 }
 
 resource "aws_route53_record" "mx" {
-  zone_id = "${aws_route53_zone.shipreq.zone_id}"
+  zone_id = aws_route53_zone.shipreq.zone_id
   name    = "shipreq.com."
   type    = "MX"
   ttl     = "3600"
@@ -28,7 +28,7 @@ resource "aws_route53_record" "mx" {
 }
 
 resource "aws_route53_record" "spf" {
-  zone_id = "${aws_route53_zone.shipreq.zone_id}"
+  zone_id = aws_route53_zone.shipreq.zone_id
   name    = "shipreq.com."
   type    = "TXT"
   ttl     = "3600"
@@ -36,7 +36,7 @@ resource "aws_route53_record" "spf" {
 }
 
 resource "aws_route53_record" "unknown_1" {
-  zone_id = "${aws_route53_zone.shipreq.zone_id}"
+  zone_id = aws_route53_zone.shipreq.zone_id
   name    = "_9a9403865e5e41bb6a547974f203fda6.shipreq.com."
   type    = "CNAME"
   ttl     = "10800"
@@ -44,7 +44,7 @@ resource "aws_route53_record" "unknown_1" {
 }
 
 resource "aws_route53_record" "gsuite_dkim" {
-  zone_id = "${aws_route53_zone.shipreq.zone_id}"
+  zone_id = aws_route53_zone.shipreq.zone_id
   name    = "google._domainkey.shipreq.com."
   type    = "TXT"
   ttl     = "21600"
@@ -52,7 +52,7 @@ resource "aws_route53_record" "gsuite_dkim" {
 }
 
 resource "aws_route53_record" "unknown_2" {
-  zone_id = "${aws_route53_zone.shipreq.zone_id}"
+  zone_id = aws_route53_zone.shipreq.zone_id
   name    = "k1._domainkey.shipreq.com."
   type    = "CNAME"
   ttl     = "3600"
@@ -60,7 +60,7 @@ resource "aws_route53_record" "unknown_2" {
 }
 
 resource "aws_route53_record" "freshdesk_dkim_1" {
-  zone_id = "${aws_route53_zone.shipreq.zone_id}"
+  zone_id = aws_route53_zone.shipreq.zone_id
   name    = "m1._domainkey.shipreq.com."
   type    = "CNAME"
   ttl     = "3600"
@@ -68,7 +68,7 @@ resource "aws_route53_record" "freshdesk_dkim_1" {
 }
 
 resource "aws_route53_record" "freshdesk_dkim_2" {
-  zone_id = "${aws_route53_zone.shipreq.zone_id}"
+  zone_id = aws_route53_zone.shipreq.zone_id
   name    = "fddkim.shipreq.com."
   type    = "CNAME"
   ttl     = "3600"
@@ -76,7 +76,7 @@ resource "aws_route53_record" "freshdesk_dkim_2" {
 }
 
 resource "aws_route53_record" "freshdesk_dkim_3" {
-  zone_id = "${aws_route53_zone.shipreq.zone_id}"
+  zone_id = aws_route53_zone.shipreq.zone_id
   name    = "s1._domainkey.shipreq.com."
   type    = "CNAME"
   ttl     = "3600"
@@ -84,7 +84,7 @@ resource "aws_route53_record" "freshdesk_dkim_3" {
 }
 
 resource "aws_route53_record" "freshdesk_dkim_4" {
-  zone_id = "${aws_route53_zone.shipreq.zone_id}"
+  zone_id = aws_route53_zone.shipreq.zone_id
   name    = "s2._domainkey.shipreq.com."
   type    = "CNAME"
   ttl     = "3600"
@@ -92,7 +92,7 @@ resource "aws_route53_record" "freshdesk_dkim_4" {
 }
 
 resource "aws_route53_record" "mailgun_1" {
-  zone_id = "${aws_route53_zone.shipreq.zone_id}"
+  zone_id = aws_route53_zone.shipreq.zone_id
   name    = "mg.shipreq.com."
   type    = "TXT"
   ttl     = "3600"
@@ -100,7 +100,7 @@ resource "aws_route53_record" "mailgun_1" {
 }
 
 resource "aws_route53_record" "mailgun_2" {
-  zone_id = "${aws_route53_zone.shipreq.zone_id}"
+  zone_id = aws_route53_zone.shipreq.zone_id
   name    = "smtp._domainkey.mg.shipreq.com."
   type    = "TXT"
   ttl     = "3600"
@@ -108,7 +108,7 @@ resource "aws_route53_record" "mailgun_2" {
 }
 
 resource "aws_route53_record" "mailgun_3" {
-  zone_id = "${aws_route53_zone.shipreq.zone_id}"
+  zone_id = aws_route53_zone.shipreq.zone_id
   name    = "mg.shipreq.com."
   type    = "MX"
   ttl     = "3600"
@@ -116,7 +116,7 @@ resource "aws_route53_record" "mailgun_3" {
 }
 
 resource "aws_route53_record" "mailgun_4" {
-  zone_id = "${aws_route53_zone.shipreq.zone_id}"
+  zone_id = aws_route53_zone.shipreq.zone_id
   name    = "email.mg.shipreq.com."
   type    = "CNAME"
   ttl     = "21600"
--- Infrastructure/terraform/shared/shared.tf
+++ Infrastructure/terraform/shared/shared.tf
@@ -1,5 +1,9 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
 provider "aws" {
-  region  = "${var.region}"
+  region  = var.region
   version = "~> 2.31"
 }
 
