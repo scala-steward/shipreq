520ec564c2d4ee20644f5d8f9c674880b862e49f
David Barri <japgolly@gmail.com>
2019-10-22 11:44:57 +1100

    Terraform: Tweak bastion prompt colours

--- Infrastructure/terraform/modules/shipreq-env/bastion-ec2-init.sh
+++ Infrastructure/terraform/modules/shipreq-env/bastion-ec2-init.sh
@@ -11,7 +11,6 @@ set -x
 
 for f in ~{root,ec2-user}/.bashrc; do
   cat >> $f << 'EOB'
-    export PS1='\n\[\e[91m[bastion-${ENV}]\e[0m \[\e[32m\]\u@\h: \[\e[33m\]\w\[\e[0m\]\n> '
     export LS_OPTIONS='--color=auto'
     alias ls='ls $LS_OPTIONS'
     alias ll='ls $LS_OPTIONS -l'
@@ -19,6 +18,12 @@ for f in ~{root,ec2-user}/.bashrc; do
     alias yy='sudo yum -y install'
 EOB
 done
+cat >> ~root/.bashrc << 'EOB'
+  export PS1='\n\[\e[95m[bastion-${ENV}]\e[0m \[\e[31m\]\u\[\e[32m\]@\h: \[\e[33m\]\w\[\e[0m\]\n> '
+EOB
+cat >> ~ec2-user/.bashrc << 'EOB'
+  export PS1='\n\[\e[95m[bastion-${ENV}]\e[0m \[\e[32m\]\u@\h: \[\e[33m\]\w\[\e[0m\]\n> '
+EOB
 
 yum -y update
 yum -y install htop nc tree
