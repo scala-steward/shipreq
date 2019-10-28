b6cd7e42a1cd05ee4085fa004bac0af200334678
David Barri <japgolly@gmail.com>
2019-10-28 21:49:34 +1100

    Exclude db.properties as a config source

--- Code/base-util/jvm/src/main/scala/shipreq/base/util/Props.scala
+++ Code/base-util/jvm/src/main/scala/shipreq/base/util/Props.scala
@@ -7,7 +7,6 @@ object Props {
 
   def fileSources: ConfigSources[Fx] =
     ConfigSource.propFileOnClasspath[Fx]("shipreq.properties", optional = true) >
-    ConfigSource.propFileOnClasspath[Fx]("db.properties", optional = true) >
     ConfigSource.propFileOnClasspath[Fx]("secret.properties", optional = true)
 
   def sources: ConfigSources[Fx] =
