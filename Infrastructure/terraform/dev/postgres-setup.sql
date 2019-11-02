4c9c07af58febb45bd59046fbd7c31635bc36566
David Barri <japgolly@gmail.com>
2019-11-02 13:38:24 +1100

    Add postgres-setup.sql for dev

---
+++ Infrastructure/terraform/dev/postgres-setup.sql
@@ -0,0 +1,25 @@
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+
+--------------------------------------------------------------------------------
+
+CREATE ROLE grafana WITH PASSWORD 'grafana' LOGIN;
+GRANT grafana TO root;
+
+CREATE DATABASE grafana OWNER grafana;
+ALTER SCHEMA public OWNER TO grafana;
+
+ALTER ROLE grafana SET search_path TO grafana;
+
+--------------------------------------------------------------------------------
+
+CREATE DATABASE shipreq;
+\c shipreq
+
+CREATE ROLE shipreq WITH PASSWORD 'dev' LOGIN;
+GRANT shipreq TO root;
+
+DROP SCHEMA public;
+CREATE SCHEMA webapp AUTHORIZATION shipreq;
+CREATE SCHEMA taskman AUTHORIZATION shipreq;
+
+ALTER ROLE shipreq SET search_path TO webapp;
