31b6a4a1d62cc931f6cc48ed1c054ba89d820508
David Barri <japgolly@gmail.com>
2019-11-02 18:23:14 +1100

    Add custom Grafana image

--- Infrastructure/terraform/global/ecr.tf
+++ Infrastructure/terraform/global/ecr.tf
@@ -17,3 +17,33 @@ resource "aws_ecr_repository" "portal" {
   name = "shipreq/ops/portal"
   tags = local.default_tags
 }
+
+resource "aws_ecr_repository" "grafana" {
+  name = "shipreq/ops/grafana"
+  tags = local.default_tags
+}
+
+resource "aws_ecr_repository" "prometheus-tech" {
+  name = "shipreq/ops/prometheus-tech"
+  tags = local.default_tags
+}
+
+resource "aws_ecr_repository" "prometheus-biz" {
+  name = "shipreq/ops/prometheus-biz"
+  tags = local.default_tags
+}
+
+resource "aws_ecr_repository" "node_exporter" {
+  name = "shipreq/ops/node_exporter"
+  tags = local.default_tags
+}
+
+resource "aws_ecr_repository" "cadvisor" {
+  name = "shipreq/ops/cadvisor"
+  tags = local.default_tags
+}
+
+resource "aws_ecr_repository" "postgres_exporter" {
+  name = "shipreq/ops/postgres_exporter"
+  tags = local.default_tags
+}
