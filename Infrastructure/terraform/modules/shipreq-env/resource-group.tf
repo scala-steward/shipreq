48d864dd4d5c818ad769dc306c69e5969069b8c1
David Barri <japgolly@gmail.com>
2019-11-29 09:07:26 +1100

    Add resource groups

---
+++ Infrastructure/terraform/modules/shipreq-env/resource-group.tf
@@ -0,0 +1,19 @@
+resource "aws_resourcegroups_group" "env" {
+  name = local.default_tags.terraform
+
+  resource_query {
+    query = <<EOB
+{
+  "ResourceTypeFilters": [
+    "AWS::AllSupported"
+  ],
+  "TagFilters": [
+    {
+      "Key": "terraform",
+      "Values": ["${local.default_tags.terraform}"]
+    }
+  ]
+}
+EOB
+  }
+}
