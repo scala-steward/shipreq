3d7d9afa5382d67868ede7a0ac642e9836fddb05
David Barri <japgolly@gmail.com>
2019-12-08 21:05:27 +1100

    Prod ops update

--- Infrastructure/terraform/prod/versions.tf
+++ Infrastructure/terraform/prod/versions.tf
@@ -1,7 +1,7 @@
 locals {
   versions = {
     nat     = "git-d3fe30d8cced3649a16ecd3fb424861de10e28d0"
-    ops     = "git-d3fe30d8cced3649a16ecd3fb424861de10e28d0"
+    ops     = "git-c0d3aea21afe262461041b5454554ba8dc0129da"
     shipreq = "git-b5d9399046dfcffc5ca6e6d74108ea97f7645932"
   }
 }
