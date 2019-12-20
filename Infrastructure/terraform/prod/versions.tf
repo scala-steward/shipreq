2da5812831dda819bf89b4b208841cfec124c4bf
David Barri <japgolly@gmail.com>
2019-12-20 12:37:06 +1100

    Prod: Upgrade ShipReq

--- Infrastructure/terraform/prod/versions.tf
+++ Infrastructure/terraform/prod/versions.tf
@@ -5,7 +5,7 @@ locals {
       cadvisor      = "git-c0d3aea21afe262461041b5454554ba8dc0129da"
       filebeat      = "git-c0d3aea21afe262461041b5454554ba8dc0129da"
       node_exporter = "git-c0d3aea21afe262461041b5454554ba8dc0129da"
-      shipreq       = "git-18d1efda2d201b7bafad7be5df40e28adf822643"
+      shipreq       = "git-eb793fc89f410fd5b28964eff2f96ed4aef2dead
     }
 
     bastion = {
