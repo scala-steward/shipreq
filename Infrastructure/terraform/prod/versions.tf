e16ba11d4f6ba0a32f530985ccf1df9f63751534
David Barri <japgolly@gmail.com>
2020-03-12 11:08:33 +1100

    Prod: Upgrade ShipReq and Bastion

--- Infrastructure/terraform/prod/versions.tf
+++ Infrastructure/terraform/prod/versions.tf
@@ -6,12 +6,12 @@ locals {
       cadvisor        = "git-c0d3aea21afe262461041b5454554ba8dc0129da"
       filebeat        = "git-8fb0c6e319fc6d4eab55bf925286d8054ba1cfac"
       node_exporter   = "git-c0d3aea21afe262461041b5454554ba8dc0129da"
-      shipreq         = "git-76419e0d1b51c54cf0cfbea7507ddf82124f1c89"
+      shipreq         = "git-ffad62b35f2a7c253b14c8332550dfe7fe8e7ef6"
     }
 
     bastion = {
-      filebeat = "git-c0d3aea21afe262461041b5454554ba8dc0129da"
-      portal   = "git-317bf685eeae06b6dc382493e6e1d6f119c857ee"
+      filebeat = "git-96d1fe4e0a6993700618b7cc936178bb05983106"
+      portal   = "git-fe6edbf3ebe0c69dda3e7fd7c3134dc461a71155"
     }
 
     nat = {
@@ -26,7 +26,7 @@ locals {
       cadvisor          = "git-c0d3aea21afe262461041b5454554ba8dc0129da"
       ecs_exporter      = "git-c0d3aea21afe262461041b5454554ba8dc0129da"
       filebeat          = "git-c0d3aea21afe262461041b5454554ba8dc0129da"
-      grafana           = "git-dbb052e5f10996e420372ad03067307a0bc5d067"
+      grafana           = "git-96d1fe4e0a6993700618b7cc936178bb05983106"
       node_exporter     = "git-c0d3aea21afe262461041b5454554ba8dc0129da"
       postgres_exporter = "git-7a5aaecd3b72ac4796faf1147b27785d9341ac16"
       prometheus_biz    = "git-c0d3aea21afe262461041b5454554ba8dc0129da"
