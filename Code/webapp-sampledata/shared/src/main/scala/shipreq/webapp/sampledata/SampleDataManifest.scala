0a84737f7a0197e52976081bd634a1f1adaca8d7
David Barri <japgolly@gmail.com>
2020-04-28 08:52:13 +1000

    Update SampleData hashcodes

--- Code/webapp-sampledata/shared/src/main/scala/shipreq/webapp/sampledata/SampleDataManifest.scala
+++ Code/webapp-sampledata/shared/src/main/scala/shipreq/webapp/sampledata/SampleDataManifest.scala
@@ -5,10 +5,10 @@ final case class SampleDataMeta(filename: String, projectConfigHash: Int, projec
 trait SampleDataManifest[D] {
   protected def load(meta: SampleDataMeta): D
 
-  lazy val  `1000`: D = load(SampleDataMeta("shipreq-events-1000.json", -15975327, -1248354367))
-  lazy val  `2000`: D = load(SampleDataMeta("shipreq-events-2000.json", 454447920, 1367503418))
-  lazy val  `4000`: D = load(SampleDataMeta("shipreq-events-4000.json", 106265614, -1464809499))
-  lazy val `10000`: D = load(SampleDataMeta("shipreq-events-10000.json", -1981610195, 1192003615))
+  lazy val  `1000`: D = load(SampleDataMeta("shipreq-events-1000.json",    -15975327, -1910444130))
+  lazy val  `2000`: D = load(SampleDataMeta("shipreq-events-2000.json",    454447920, -1851467186))
+  lazy val  `4000`: D = load(SampleDataMeta("shipreq-events-4000.json",    106265614, -1357390324))
+  lazy val `10000`: D = load(SampleDataMeta("shipreq-events-10000.json", -1981610195, -1390028356))
 
   lazy val all: Vector[D] =
     Vector(
