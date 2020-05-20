724acea2bf857524259ec4fc6dc56cc1dd4df070
David Barri <japgolly@gmail.com>
2020-05-20 17:38:34 +1000

    Update ZIO in BM

--- Code/project/ShipReqBuild.scala
+++ Code/project/ShipReqBuild.scala
@@ -118,7 +118,7 @@ object ShipReqBuild {
         .dependsOn(webappServer)
         .configure(Common.jvmSettings)
         .settings(libraryDependencies ++= Seq(
-          "dev.zio" %% "zio" % "1.0.0-RC18-2"))
+          "dev.zio" %% "zio" % "1.0.0-RC19"))
         .deps(JJWT.all)
     }
 
