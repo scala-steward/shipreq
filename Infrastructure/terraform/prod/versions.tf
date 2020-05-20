19c806cc47efb823a9c307bb3c563d6afb147661
David Barri <japgolly@gmail.com>
2020-05-20 21:33:24 +1000

    Upgrade prod services

--- Infrastructure/terraform/prod/versions.tf
+++ Infrastructure/terraform/prod/versions.tf
@@ -2,9 +2,9 @@ locals {
   versions = {
 
     app = {
-      analytics_proxy = "git-56cf0959cb10f95fe6bdaf906ddfe0f18a313191"
+      analytics_proxy = "git-10f2f46aad4df3ea32622dad2ff3e8eef4d0a81a"
       cadvisor        = "git-c0d3aea21afe262461041b5454554ba8dc0129da"
-      filebeat        = "git-8fb0c6e319fc6d4eab55bf925286d8054ba1cfac"
+      filebeat        = "git-10f2f46aad4df3ea32622dad2ff3e8eef4d0a81a"
       node_exporter   = "git-c0d3aea21afe262461041b5454554ba8dc0129da"
       shipreq         = "git-ffad62b35f2a7c253b14c8332550dfe7fe8e7ef6"
     }
@@ -25,12 +25,12 @@ locals {
     ops = {
       cadvisor          = "git-c0d3aea21afe262461041b5454554ba8dc0129da"
       ecs_exporter      = "git-c0d3aea21afe262461041b5454554ba8dc0129da"
-      filebeat          = "git-c0d3aea21afe262461041b5454554ba8dc0129da"
+      filebeat          = "git-10f2f46aad4df3ea32622dad2ff3e8eef4d0a81a"
       grafana           = "git-96d1fe4e0a6993700618b7cc936178bb05983106"
       node_exporter     = "git-c0d3aea21afe262461041b5454554ba8dc0129da"
       postgres_exporter = "git-7a5aaecd3b72ac4796faf1147b27785d9341ac16"
-      prometheus_biz    = "git-c0d3aea21afe262461041b5454554ba8dc0129da"
-      prometheus_tech   = "git-c0d3aea21afe262461041b5454554ba8dc0129da"
+      prometheus_biz    = "git-10f2f46aad4df3ea32622dad2ff3e8eef4d0a81a"
+      prometheus_tech   = "git-10f2f46aad4df3ea32622dad2ff3e8eef4d0a81a"
     }
 
   }
