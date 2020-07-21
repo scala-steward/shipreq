6daffa6d25f511cb1c0727406f7b1b3f280e6058
David Barri <japgolly@gmail.com>
2020-07-21 11:54:14 +1000

    Add gatsby-plugin-catch-links

--- Web/tech/gatsby-config.js
+++ Web/tech/gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   plugins: [
 
     // =============================================================================================
+    "gatsby-plugin-catch-links",
     "gatsby-plugin-mdx",
     "gatsby-plugin-svgr-svgo",
 
