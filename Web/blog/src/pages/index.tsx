e5096ca42ff303cef63c1c80f2dd537f235c1292
David Barri <japgolly@gmail.com>
2020-07-25 20:29:02 +1000

    Sort index by latest first

--- Web/blog/src/pages/index.tsx
+++ Web/blog/src/pages/index.tsx
@@ -4,7 +4,7 @@ import React from "react"
 
 export const pageQuery = graphql`
   query {
-    allMdx {
+    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
       edges {
         node {
           id
