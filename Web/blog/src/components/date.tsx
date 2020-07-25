d9c383eb4287c8c8215905a19163177f6087e2ce
David Barri <japgolly@gmail.com>
2020-07-25 22:59:07 +1000

    Fix layout jitter in date

--- Web/blog/src/components/date.tsx
+++ Web/blog/src/components/date.tsx
@@ -2,6 +2,8 @@ import { Props } from "./client-side/date"
 import loadable from "@loadable/component"
 import React from "react"
 
-const LazyDate = loadable(() => import("./client-side/date"))
+const fallback = <span style={{visibility: "hidden"}}>loading</span>
+
+const LazyDate = loadable(() => import("./client-side/date"), { fallback })
 
 export default (p: Props) => <LazyDate {...p} />
