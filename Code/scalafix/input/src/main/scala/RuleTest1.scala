286b0b38974e428b6b3a20e06288f9b3ee40d085
David Barri <japgolly@gmail.com>
2020-08-25 17:58:13 +1000

    Add a scalafix rule for CustomField.Tag.Id#some

--- Code/scalafix/input/src/main/scala/RuleTest1.scala
+++ Code/scalafix/input/src/main/scala/RuleTest1.scala
@@ -1,6 +1,26 @@
 /*
 rule = FixShipReq
 */
+import shipreq.webapp.base.data.CustomField
+
 object RuleTest1 {
-  // Add code that needs fixing here.
+
+  val tag = CustomField.Tag.Id(1)
+  Option(tag)
+  Some(tag)
+  Option.apply(tag)
+  Some.apply(tag)
+
+  def tagFn(i: Int) = CustomField.Tag.Id(i)
+  Option(tagFn(1))
+  Some(tagFn(2))
+  Option.apply(tagFn(3))
+  Some.apply(tagFn(4))
+
+  val text = CustomField.Text.Id(1)
+  Option(text)
+  Some(text)
+  Option.apply(text)
+  Some.apply(text)
+
 }
--- Code/scalafix/rules/src/main/scala/shipreq/fix/FixShipReq.scala
+++ Code/scalafix/rules/src/main/scala/shipreq/fix/FixShipReq.scala
@@ -1,15 +1,46 @@
 package shipreq.fix
 
 import scalafix.v1._
-//import scala.meta._
+import scala.meta._
+import Helpers._
 
-class FixShipReq extends SemanticRule("FixShipReq") {
+object FixShipReq {
 
-  override def fix(implicit doc: SemanticDocument): Patch = {
-//    println("Tree.syntax: " + doc.tree.syntax)
-//    println("Tree.structure: " + doc.tree.structure)
-//    println("Tree.structureLabeled: " + doc.tree.structureLabeled)
-    Patch.empty
+  val OptionOrSomeApply = SymbolMatcher.exact("scala/Option.apply().", "scala/Some.apply().").ignoreErrors
+
+  object OptionOrSomeTermName {
+    def unapply(t: Term.Name) = t.value == "Option" || t.value == "Some"
   }
 
+  val CustomFieldTagId = SymbolMatcher.exact("shipreq/webapp/base/data/CustomField.Tag.Id#")
+}
+
+class FixShipReq extends SemanticRule("FixShipReq") {
+  import FixShipReq._
+
+  override def fix(implicit doc: SemanticDocument): Patch = {
+//    println("Tree.syntax\n" + doc.tree.syntax)
+//    println("Tree.structure\n" + doc.tree.structure)
+//    println("Tree.structureLabeled\n" + doc.tree.structureLabeled)
+
+    doc.tree.collect {
+
+      // {Option,Some}(arg)
+      case tree @ Term.Apply(option @ OptionOrSomeTermName(), arg :: Nil) =>
+        fixOptionOrSomeApply(tree, option.synthetics, arg)
+
+      // {Option,Some}.apply(arg)
+      case tree @ OptionOrSomeApply(Term.Apply(option @ Term.Select(_, _), arg :: Nil)) =>
+        fixOptionOrSomeApply(tree, option.synthetics, arg)
+
+    }.asPatch
+  }
+
+  private def fixOptionOrSomeApply(tree: Tree, option: List[SemanticTree], arg: Term): Patch = {
+//    println(option.headOption.structure)
+    if (option.headOption.exists(_.arg1SymbolMatches(CustomFieldTagId)))
+      Patch.replaceTree(tree, arg.toString + ".some")
+    else
+      Patch.empty
+  }
 }
