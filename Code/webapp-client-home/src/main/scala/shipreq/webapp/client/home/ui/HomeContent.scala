64c020f52b2683bcfbfb73411a5d222efe489e43
David Barri <japgolly@gmail.com>
2026-06-05 12:25:10 +1000

    Refactor: Extract `HomeContent` in its own file

---
+++ Code/webapp-client-home/src/main/scala/shipreq/webapp/client/home/ui/HomeContent.scala
@@ -0,0 +1,75 @@
+package shipreq.webapp.client.home.ui
+
+import japgolly.scalajs.react._
+import japgolly.scalajs.react.extra._
+import japgolly.scalajs.react.vdom.html_<^._
+import scalacss.ScalaCssReact._
+import shipreq.base.util.ErrorMsg
+import shipreq.webapp.base.config.WebappConfig
+import shipreq.webapp.base.feature.{AsyncFeature, EditorStatus}
+import shipreq.webapp.base.ui.semantic.Colour
+import shipreq.webapp.base.ui.widgets._
+import shipreq.webapp.member.project.data.{DataValidators, ProjectMetaData}
+import shipreq.webapp.member.ui._
+
+object HomeContent {
+
+  final case class Props(projects         : List[ProjectMetaData],
+                         createProjectText: StateSnapshot[String],
+                         createProjectAS  : AsyncFeature.Read.D0[ErrorMsg],
+                         createProjectIO  : String => Callback,
+                         tagMod           : TagMod) {
+    @inline def render = Component(this)
+  }
+
+  final class Backend {
+
+    val inputMod: TagMod =
+      TagMod(^.placeholder := "New project name", Styles.createProjectInput)
+
+    def render(p: Props): VdomElement = {
+
+      val noProjects = p.projects.isEmpty
+
+      val createProject = {
+        val status: EditorStatus =
+          EditorStatus.async(p.createProjectAS) getOrElse
+            EditorStatus.ignoreOrValidate(DataValidators.projectName.unnamed)(
+              p.createProjectText.value, _.isEmpty, s => Some(p.createProjectIO(s)))
+        <.div(Styles.createProjectCont,
+          PlainTextEditor.WithButton.Props(
+            p.createProjectText.value,
+            p.createProjectText.setState,
+            status,
+            Colour.Green,
+            buttonLabel = "Create Project",
+            inputMod = TagMod(inputMod, (^.autoFocus := true).when(noProjects)))
+            .render)
+      }
+
+      def noProjectGreeting: VdomTag =
+        <.div(Styles.noProjects,
+          NoContentMessage(
+            s"Welcome to ${WebappConfig.appName}!",
+            TagMod(
+              "The first thing you'll want to do is create a project to contain all of your requirements.",
+              <.br,
+              "Create a new project using the button above.")))
+
+      def projectList: VdomTag =
+        <.div(Styles.projectList,
+          p.projects.sortBy(_.name).toTagMod(p => ProjectItem.AsLink.Component.withKey(p.id.value)(p)))
+
+      <.main(
+        BaseStyles.containerLarge,
+        p.tagMod,
+        createProject,
+        if (noProjects) noProjectGreeting else projectList)
+    }
+  }
+
+  val Component = ScalaComponent.builder[Props]
+    .renderBackend[Backend]
+    .build
+
+}
